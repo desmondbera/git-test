171ac8646854aba79ee602917ca4ec7838282e38
Desmond Beramendi <desmond.bera@gmail.com>
2019-05-25 17:31:59 -0400

    Initial commit for Git Test.

---
+++ GitTestProj/src/com/desmond/git_test_two/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.desmond.git_test_two;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO GITHUBBBBB!");
+	}
+
+}
